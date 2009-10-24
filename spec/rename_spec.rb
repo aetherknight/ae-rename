a7d82cc93ed5e6463bfd601878127e4f05679ef3
William (B.J.) Snow Orvis <aetherknight@gmail.com>
2009-10-24 13:52:53 -0700

    Rename should rename a file 'a' to 'b' when invoked as `rename 'a' 'b' a`

--- spec/rename_spec.rb
+++ spec/rename_spec.rb
@@ -2,9 +2,20 @@
 require "pty"
 require "expect"
 
-RENAME = "bin/rename"
+RENAME = "../bin/rename"
+TESTDIR = "testdir"
 
 describe RENAME do
+  before(:each) do
+    @currdir = Dir.getwd
+    Dir.mkdir(TESTDIR)
+    Dir.chdir(TESTDIR)
+  end
+
+  after(:each) do
+    Dir.chdir(@currdir)
+    `rm -rf #{TESTDIR}`
+  end
 
   it "should print its usage if it receives no arguments" do
     PTY.spawn("#{RENAME}") do |stdin, stdout, pid|
@@ -19,4 +30,29 @@ describe RENAME do
       stdin.expect(/rename (.*)\r\n/).should_not == nil
     end
   end
+
+  it "should rename a file 'a' to 'b' when invoked as `rename 'a' 'b' a`" do
+    `touch a`
+    Dir.entries('.').should be_include('a')
+    Dir.entries('.').should_not be_include('b')
+
+    PTY.spawn("#{RENAME} 'a' 'b' a") do |stdin, stdout, pid|
+      stdin.expect("").should == nil
+    end
+    Dir.entries('.').should_not be_include('a')
+    Dir.entries('.').should be_include('b')
+  end
+
+#  it "should rename a file 'b' to 'c' when invoked as `rename 'b' 'c' b`" do
+#    `touch a`
+#    Dir.entries('.').should be_include('b')
+#    Dir.entries('.').should_not be_include('c')
+#
+#    PTY.spawn("#{RENAME} 'b' 'c' b") do |stdin, stdout, pid|
+#      stdin.expect("").should == nil
+#    end
+#    Dir.entries('.').should_not be_include('b')
+#    Dir.entries('.').should be_include('c')
+#  end
+
 end
